6ec5d40356e4d9b75abec2054f29fa14d3e1e398
codrat <codratz@gmail.com>
2015-06-29 12:47:07 +0000

    first commit

---
+++ BoltNetwork2/js/controllers/MainController.js
@@ -0,0 +1,22 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.program=[
+    {
+    series: "Sherlock",
+    series_img: "img/sherlock.jpg",
+    genre: "Crime drama",
+    season: 3,
+    episode: "The Empty Hearse",
+    description: "Two years after his reported Reichenbach Fall demise, Sherlock, who has been cleared of all fraud charges against him, returns with Mycroft's help to a London under threat of terrorist attack. John has moved on and has a girlfriend, Mary Morstan. Sherlock enlists Molly to assist him, but when John is kidnapped by unknown assailants and is rescued by Sherlock and Mary, John returns to help find the terrorists and an underground plot to blow up the Houses of Parliament during an all night sitting on Guy Fawkes Night.",
+  datetime: new Date(2014, 11, 31, 21, 00, 00, 00)
+},
+{
+  series: "Muie",
+  series_img: "http://i1.rgstatic.net/i/profile/ef3175501c8a3d8bfc_l_1bccc.jpg",
+  genre: "pornachi",
+  season: 69,
+  episode: "mamaluicioaraanalsexxxx",
+  description: "Mama lui cioara isi ia la gaoz PUTERNIC",
+  datetime: new Date(2015, 06, 20, 00, 17, 00, 00)
+}
+];
+}]);
